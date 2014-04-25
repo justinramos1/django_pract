d79e26f8850ae0723c66bc77ae177bb859fa411a
justinramos1 <jramos@resortmanagementsystem.com>
2014-04-25 18:00:17 -0400

    upload

---
+++ mysite/polls/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Poll(models.Model):
+	question = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date_published')
+
+	def was_published_yesterday(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+	def __str__(self):
+		return self.question
+
+class Choice(models.Model):
+	poll = models.ForeignKey(Poll)
+	choice_text = models.CharField(max_length=200)
+	votes = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.choice_text
